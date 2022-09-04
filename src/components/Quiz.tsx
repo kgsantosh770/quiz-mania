a3f5d8601cf94c10729f2fcb0bb90cb270b5cc58
Santosh <s.kunga.ganeshbabu@accenture.com>
2022-09-04 14:31:14 +0530

    fetch data from api & parse string to html

--- src/components/Quiz.tsx
+++ src/components/Quiz.tsx
@@ -1,35 +1,55 @@
+import { wait } from '@testing-library/user-event/dist/utils'
 import React, { useEffect, useState } from 'react'
 import "../scss/Quiz.scss"
-import quizData from "../QuestionData"
+// import quizData from "../QuestionData"
 import SingleQuiz from './SingleQuiz'
 
 function Quiz() {
 
-    const [allQuiz, setAllQuiz] = useState(quizData)
+    const [allQuiz, setAllQuiz] = useState([] as any)
     const [revealAnswers, setRevealAnswers] = useState(false)
+    const [loading, setLoading] = useState(true)
 
-    const quizes = allQuiz.map((quiz: any) => {
-        return(
-        <SingleQuiz
-            key={quiz.id}
-            quiz={quiz}
-            revealAnswers={revealAnswers}
-        />
-    )})
+    useEffect(() => {
+        if (revealAnswers === false) {
+            fetch("https://opentdb.com/api.php?amount=10&category=9&difficulty=easy")
+                .then(res => res.json())
+                .then(data => setAllQuiz(data.results))
+                .then(() => setLoading(false))
+        }
+    }, [revealAnswers])
+
+    const quizes = allQuiz.map((quiz: any, index: number) => {
+        console.log("dammaaal")
+        return (
+            <SingleQuiz
+                key={index}
+                quiz={quiz}
+                revealAnswers={revealAnswers}
+            />
+        )
+    })
 
     function checkAnswers() {
-        if(revealAnswers)
+        if (revealAnswers){
             setRevealAnswers(false)
+            setLoading(true)
+        }
         else
             setRevealAnswers(true)
     }
 
     return (
         <div className='quiz-component'>
-            {quizes}
-            <button className="submit-quiz-btn" onClick={checkAnswers}>
-                {revealAnswers ? "New Mania" : "Check Answers"}
-            </button>
+            {loading ? <div>loading...</div> :
+                <>
+                    {quizes}
+                    <button className="submit-quiz-btn" onClick={checkAnswers}>
+                        {revealAnswers ? "New Mania" : "Check Answers"}
+                    </button>
+                </>
+            }
+
         </div>
     )
 }
--- src/components/SingleQuiz.tsx
+++ src/components/SingleQuiz.tsx
@@ -1,44 +1,52 @@
 import React, { useEffect, useState } from 'react'
+import parse from 'html-react-parser';
 import "../scss/SingleQuiz.scss"
 
 function SingleQuiz(props: any) {
-
-    const [selectedOption, setSelectedOption] = useState("")    
+    const [selectedOption, setSelectedOption] = useState("")
+    const [allOptions, setAllOptions] = useState([] as Array<string>)
 
     useEffect(() => {
-        if(props.revealAnswers === false)
+        if (props.revealAnswers === false) {
+            const randomIndex = Math.ceil(Math.random() * 4)
+            const allOptionsArray = props.quiz.incorrect_answers.slice(0, randomIndex)
+            allOptionsArray.push(props.quiz.correct_answer)
+            const remainingOptions = props.quiz.incorrect_answers.slice(randomIndex)
+            if(remainingOptions.length > 0)
+                allOptionsArray.push(...remainingOptions)
+            setAllOptions(allOptionsArray)
             setSelectedOption("")
-    }, [props.revealAnswers])
-    
+        }
+    }, [props.revealAnswers, props.quiz.incorrect_answers])
+
 
     function optionColor(option: String) {
-        const isOptionCorrect = props.quiz.options[props.quiz.correctOption-1] === option ? true : false
-        if(isOptionCorrect)
+        const isOptionCorrect = props.quiz.correct_answer === option ? true : false
+        if (isOptionCorrect)
             return "green"
-        else if(selectedOption === option)
+        else if (selectedOption === option)
             return "red"
         else
             return ""
     }
 
-    const options = props.quiz.options.map((option: string, index: number) => {
+    console.log(allOptions)
+
+    const options = allOptions.map((option: string, index: number) => {
         const isSelected = selectedOption === option ? true : false
         const color = optionColor(option);
         return <button
             key={index}
-            className={`option ${isSelected ? "selected": ""} ${props.revealAnswers ? color : ""}`}
-            onClick={()=>{
-                console.log(option)
-                setSelectedOption(option)
-            }}
+            className={`option ${isSelected ? "selected" : ""} ${props.revealAnswers ? color : ""}`}
+            onClick={() => { setSelectedOption(option) }}
         >
-            {option}
+            {parse(option)}
         </button>
     })
 
     return (
         <div className="singlequiz-component">
-            <p className="question">{props.quiz.question}</p>
+            <p className="question">{parse(props.quiz.question)}</p>
             <div className={`options ${props.revealAnswers ? "revealed" : ""}`}>
                 {options}
             </div>
